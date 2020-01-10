eae4d0fc89090602307f7cf973273b77f85a3276
Ratna Mandava <ratnapremamandava@gmail.com>
2020-01-10 12:59:26 -0500

    Add mock data to the clients page

--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -1,45 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
+import clients from '../../clients';
+
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.scss']
 })
 export class ClientsComponent implements OnInit {
- clientItems: any;
+ clients: any;
 
   constructor(private router: Router) {
   }
 
   ngOnInit() {
 
-    this.clientItems = [
-      {
-        clientName: 'CLIENT NAME',
-        clientEmail: 'abc@xyz.com',
-        clientPhone: '1234567890',
-        clientCity: 'Miami'
-      },
-      {
-        clientName: 'CLIENT NAME',
-        clientEmail: 'qwerty@xyz.com',
-        clientPhone: '2345678901',
-        clientCity: 'New York'
-      },
-      {
-        clientName: 'CLIENT NAME',
-        clientEmail: 'asdfg@xyz.com',
-        clientPhone: '3456789012',
-        clientCity: 'California'
-      },
-      {
-        clientName: 'CLIENT NAME',
-        clientEmail: 'zxcvbn@xyz.com',
-        clientPhone: '4567890123',
-        clientCity: 'Dallas'
-      },
-    ];
+    this.clients = clients;
+    console.log(`clients: `, clients);
   }
 
   onAddClient(){
