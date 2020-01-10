30c87d96592dc6c2fd09ae87e1799d162f9260d5
Ratna Mandava <ratnapremamandava@gmail.com>
2020-01-10 13:26:29 -0500

    Check-in clients mock data.

---
+++ src/app/clients.ts
@@ -0,0 +1,54 @@
+let clients = [{
+  'clientName': 'Roberts-Reynolds',
+  'clientEmail': 'apellamonuten0@4shared.com',
+  'clientPhone': '224-233-9857',
+  'clientCity': 'Kalodu'
+}, {
+  'clientName': 'Monahan-Hills',
+  'clientEmail': 'bnewvell1@scribd.com',
+  'clientPhone': '700-169-4403',
+  'clientCity': 'Urdaneta'
+}, {
+  'clientName': 'Hartmann Inc',
+  'clientEmail': 'ideplacido2@ask.com',
+  'clientPhone': '681-826-8592',
+  'clientCity': 'Socorro'
+}, {
+  'clientName': 'Wintheiser-Hammes',
+  'clientEmail': 'agregorin3@answers.com',
+  'clientPhone': '300-782-0610',
+  'clientCity': 'Vegachí'
+}, {
+  'clientName': 'Torphy, Skiles and Bosco',
+  'clientEmail': 'jlarver4@icio.us',
+  'clientPhone': '598-938-2563',
+  'clientCity': 'Klwów'
+}, {
+  'clientName': 'Lueilwitz and Sons',
+  'clientEmail': 'kcurrington5@indiegogo.com',
+  'clientPhone': '484-226-5052',
+  'clientCity': 'Nampera'
+}, {
+  'clientName': 'Hessel, O\'Conner and Morar',
+  'clientEmail': 'bkarlik6@google.com.br',
+  'clientPhone': '371-933-2032',
+  'clientCity': 'Bukitkemuning'
+}, {
+  'clientName': 'Eichmann, Breitenberg and Thompson',
+  'clientEmail': 'lyarwood7@soup.io',
+  'clientPhone': '995-313-4367',
+  'clientCity': 'Gangsha'
+}, {
+  'clientName': 'Anderson LLC',
+  'clientEmail': 'mleavey8@lulu.com',
+  'clientPhone': '865-302-6585',
+  'clientCity': 'Ciudad Choluteca'
+}, {
+  'clientName': 'Nienow, Kreiger and Mayert',
+  'clientEmail': 'echatwin9@apache.org',
+  'clientPhone': '631-342-8597',
+  'clientCity': 'Nagasaki-shi'
+}];
+
+
+export default clients;
